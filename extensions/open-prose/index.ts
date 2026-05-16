6ebdf799f5a4479a425827f7dd86c4c998a01657
Matthew Overing <contact@smithersbot.com>
2026-05-16 16:42:41 -0400

    claw: prune-non-channel-extensions

--- extensions/open-prose/index.ts
+++
@@ -1,5 +0,0 @@
-import type { MoltbotPluginApi } from "../../src/plugins/types.js";
-
-export default function register(_api: MoltbotPluginApi) {
-  // OpenProse is delivered via plugin-shipped skills.
-}
